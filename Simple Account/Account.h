fcdf71df6b70ad277f5d9e0a38ae3f9638305265
abcLiyew <18378500963@163.com>
2023-05-31 12:49:37 +0800

    类ACCount的声明

--- Simple Account/Account.h
+++ Simple Account/Account.h
@@ -2,8 +2,23 @@
 #include<fstream>
 #include<string>
 #include<cmath>
+using namespace std;
 
 class Account
 {
+public:
+    Account():name("张三"),Accout("abcdefg"),password("123456"),banlance(0){}
+    Account(string n,string Acc,string p):name(n),Accout(Acc),password(p),banlance(0){}
+    Account(string n,string Acc,string p,float b):name(n),Accout(Acc),password(p),banlance(b){}
+    void display();//输出名字账号密码余额
+    bool check();//检查账号密码
+    bool deposit();//存款
+    bool drawing();//取款
+
+private:
+    string name;
+    string Accout;
+    string password;
+    float banlance;
     
 };
